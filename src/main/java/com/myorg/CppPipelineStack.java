f51b1920191fd49b84c598bdf5df52345435be86
Bo Chen <bochen@BodeMacBook-Air.local>
2025-08-23 22:52:56 -0700

    Update CDK and Lambda

--- src/main/java/com/myorg/CppPipelineStack.java
+++ src/main/java/com/myorg/CppPipelineStack.java
@@ -4,14 +4,15 @@ import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.codebuild.*;
-import software.amazon.awscdk.services.codecommit.Repository;
 import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.constructs.Construct;
 import software.amazon.awscdk.services.codepipeline.*;
 import software.amazon.awscdk.services.codepipeline.actions.*;
+import software.amazon.awscdk.services.codebuild.ComputeType;
 
 
 import java.util.List;
+import software.amazon.awscdk.SecretValue;
 
 public class CppPipelineStack extends Stack {
     public CppPipelineStack(final Construct scope, final String id) {
@@ -21,20 +22,14 @@ public class CppPipelineStack extends Stack {
     public CppPipelineStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // 1 创建 codeCommit 仓库
-        Repository repo = Repository.Builder.create(this, "CppRepo")
-                .repositoryName("cpp-repo")
-                .build();
-
-        // 2 定义 codeBuild 项目
+        // 1. 定义 CodeBuild 项目
         PipelineProject buildProject = PipelineProject.Builder.create(this, "CppBuilderProject")
                 .buildSpec(BuildSpec.fromObject(new java.util.HashMap<String, Object>() {{
                     put("version", "0.2");
                     put("phases", new java.util.HashMap<String, Object>() {{
                         put("install", new java.util.HashMap<String, Object>() {{
                             put("commands", List.of(
-                                    "echo Installing Maven...",
-                                    "yum install -y maven"
+                                    "echo Skipping Maven install because image already has it"
                             ));
                         }});
                         put("build", new java.util.HashMap<String, Object>() {{
@@ -49,34 +44,41 @@ public class CppPipelineStack extends Stack {
                         put("files", List.of("**/*"));
                     }});
                 }}))
+                .buildSpec(BuildSpec.fromSourceFilename("buildspec.yml"))
                 .environment(BuildEnvironment.builder()
-                        .buildImage(LinuxBuildImage.STANDARD_6_0)
+                        .buildImage(LinuxBuildImage.STANDARD_5_0)
                         .computeType(ComputeType.SMALL)
                         .build())
                 .timeout(Duration.minutes(30))
                 .build();
 
-        // 3. CodeBuild 权限
+        // 2. CodeBuild 权限
         buildProject.addToRolePolicy(PolicyStatement.Builder.create()
                 .actions(List.of("cloudformation:*", "s3:*", "lambda:*", "dynamodb:*", "apigateway:*", "iam:*"))
                 .resources(List.of("*"))
                 .build());
 
-        // 4. 创建 Pipeline
+        // 3. 创建 Pipeline
         Artifact sourceOutput = new Artifact();
         Artifact buildOutput = new Artifact();
 
+        // 4. 使用 GitHub 作为 Source
+        CodeStarConnectionsSourceAction githubSource = CodeStarConnectionsSourceAction.Builder.create()
+                .actionName("GitHub_Source")
+                .owner("bochen0214")  // GitHub 用户名
+                .repo("cpp-cdk")      // 仓库名
+                .branch("main") // 分支
+                .connectionArn("arn:aws:codeconnections:us-east-2:235494800347:connection/d7bbb5ef-bfbd-4054-8c5f-2ca41a148d67")
+                .output(sourceOutput)
+                .build();
+
+        // 5. 构建 Pipeline
         Pipeline pipeline = Pipeline.Builder.create(this, "CppPipeline")
                 .pipelineName("CppPipeline")
                 .stages(List.of(
                         StageProps.builder()
                                 .stageName("Source")
-                                .actions(List.of(CodeCommitSourceAction.Builder.create()
-                                        .actionName("CodeCommit")
-                                        .repository(repo)
-                                        .output(sourceOutput)
-                                        .branch("main")
-                                        .build()))
+                                .actions(List.of(githubSource))
                                 .build(),
                         StageProps.builder()
                                 .stageName("BuildAndDeploy")
